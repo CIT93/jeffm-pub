c02bed456d8672041661c6e3b1dc822aea4462d0
Jeffzyxw <jeffleft@live.com>
2024-01-19 01:28:00 -0800

    fixing code

--- w2/main.js
+++ w2/main.js
@@ -1,17 +1,14 @@
-const myHeading = document.querySelector("h1");
-myHeading.textContent = "Hello world!";
+const carbonFoot1 = 8;
+const carbonFoot2 = 7;
+const carbonFoot3 = 3;
+const carbonFoot4 = 2;
+const carbonFoot5 = 2;
+const carbonFoot6 = 5;
+const carbonFoot7 = 18;
+const carbonFoot8 = 4;
 
-// single line comment
-/* block or multi line comment */
 
-// coding challenge - will this work code?
-// No this code will not work; because the index.html page has h1. And the main.js has h2.
+const total = carbonFoot1 + carbonFoot2 + carbonFoot3 + carbonFoot4 + carbonFoot5 + carbonFoot6 + carbonFoot7 + carbonFoot8
 
-
-// naming standard is called camelCase is just in this case
-
-const myNumber = 42;
-let myString = "Hello";
-// myString = 43; not a good idea!
-// general code for variable declaration use const
-// use const and let not var do not use var var has problems with hoisting
+const myHeading = document.querySelector("h2");
+myHeading.textContent = total;
