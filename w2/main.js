08874ba755f58d4c8c29efef9b4a3aef222cb48a
Jeffzyxw <jeffleft@live.com>
2024-01-19 01:14:58 -0800

    fixing bad code

--- w2/main.js
+++ w2/main.js
@@ -1,15 +1,17 @@
-const carbonFoot1 = 8;
-const carbonFoot2 = 7;
-const carbonFoot3 = 3;
-const carbonFoot4 = 2;
-const carbonFoot5 = 2;
-const carbonFoot6 = 5;
-const carbonFoot7 = 18;
-const carbonFoot8 = 4;
-const total = document.querySelector("h2");
-total.textContent = carbonFoot1 + carbonFoot2 + carbonFoot3 + carbonFoot4 + carbonFoot5 + carbonFoot6 + carbonFoot7 + carbonFoot8;
+const myHeading = document.querySelector("h1");
+myHeading.textContent = "Hello world!";
+
+// single line comment
+/* block or multi line comment */
+
+// coding challenge - will this work code?
+// No this code will not work; because the index.html page has h1. And the main.js has h2.
 
 
-// above is 8 questions about my carbon footprint https://www.wikihow.com/Calculate-Your-Carbon-Footprint 20240118 
+// naming standard is called camelCase is just in this case
 
-
+const myNumber = 42;
+let myString = "Hello";
+// myString = 43; not a good idea!
+// general code for variable declaration use const
+// use const and let not var do not use var var has problems with hoisting
---
+++ w2/mainscratcj.js
@@ -0,0 +1,15 @@
+const carbonFoot1 = 8;
+const carbonFoot2 = 7;
+const carbonFoot3 = 3;
+const carbonFoot4 = 2;
+const carbonFoot5 = 2;
+const carbonFoot6 = 5;
+const carbonFoot7 = 18;
+const carbonFoot8 = 4;
+const total = document.querySelector("h2");
+total.textContent = carbonFoot1 + carbonFoot2 + carbonFoot3 + carbonFoot4 + carbonFoot5 + carbonFoot6 + carbonFoot7 + carbonFoot8;
+
+
+// above is 8 questions about my carbon footprint https://www.wikihow.com/Calculate-Your-Carbon-Footprint 20240118 
+
+
