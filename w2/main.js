a666a999cd5fcc4377c1613013dfc87b613f8a3d
Jeffzyxw <jeffleft@live.com>
2024-08-21 07:39:17 -0700

    setup done

--- w2/main.js
+++ w2/main.js
@@ -1,1 +1,51 @@
 console.log("Hope This Works")
+
+/*1
+Count the members of your household. 
+14
+*/
+
+/*2
+Consider the size of your home.
+7
+*/
+
+
+
+/*3
+Evaluate your food choices.
+4
+*/
+
+
+/*4
+Examine your water consumption.
+4
+*/
+
+
+/*5
+Determine how many household purchases you make each year.
+4
+*/
+
+/*6
+Consider how much waste you produce. The more trash 
+5
+*/
+
+/*7
+Identify the amount of waste that you recycle. 
+0
+*/
+
+
+/*8
+Tally up your annual transportation scores. 
+6
+*/
+
+/*9
+Add up your points.
+44
+*/
