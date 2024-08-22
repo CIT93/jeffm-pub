ae6ad2004035903036e1ef6ba245f72b695efdad
Jeffzyxw <jeffleft@live.com>
2024-08-22 05:51:03 -0700

    messing around

--- w3/main.js
+++ w3/main.js
@@ -2,17 +2,16 @@ console.log("Hello from inside the main.js file");
 
 // let myVar;
 
-const myVar = "Jeff";
+const myVar = "46";
 const myVarType = typeof myVar;
-
-console.log(`myVarType: ${myVarType}`);
-console.log(`${myVarType}`);
+console.log("myVarType " = myVarType);
+console.log(`myVarType ${myVarType}`);
 
 function runNow() {
     if (myVarType === "number") {
-        console.log("Will this one run?");
+        console.log(`Will this one not run?`);
     } else {
-        console.log("Will this one run?");
+        console.log(`Will this one run?`);
     }
 }
 
