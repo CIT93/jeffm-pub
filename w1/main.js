0308f60ff7503cc2a520f5ce8db25b9e5c9eb14d
Jeffzyxw <jeffleft@live.com>
2024-08-22 05:52:18 -0700

    adding code

--- w1/main.js
+++ w1/main.js
@@ -1,1 +1,26 @@
-console.log("Hello World!")
+console.log("Hello World!");
+console.log("Hello from inside the main.js file");
+
+// let myVar;
+
+const myVar = "46";
+const myVarType = typeof myVar;
+console.log("myVarType " = myVarType);
+console.log(`myVarType ${myVarType}`);
+
+function runNow() {
+    if (myVarType === "number") {
+        console.log(`Will this one not run?`);
+    } else {
+        console.log(`Will this one run?`);
+    }
+}
+
+runNow();
+runNow();
+
+
+// to update html with total (const carbonPrintTotal)
+const myVar = document.querySelector("h2");
+// to update index.html
+myVar.textContent = myVarType;
