0026e7e9904a8637d75da8158cb9d64626a95206
Jeffzyxw <jeffleft@live.com>
2024-09-06 00:38:46 -0700

    setup done

---
+++ w4/main.js
@@ -0,0 +1,68 @@
+const cfpData = [];
+
+function determineHouseSizePts(size) {
+  //let houseSizePoints = 0;
+  if (size === "large") {
+    houseSizePoints = 10;
+  } else if (size === "medium") {
+    houseSizePoints = 7;
+  } else if (size === "small") {
+    houseSizePoints = 4;
+  } else if (size === "apt") {
+    houseSizePoints = 2;
+  }
+
+  return houseSizePoints;
+}
+
+function determineHouseHoldPts(numberInHousehold) {
+  //let houseHoldPoints = 0;
+  if (numberInHousehold === 1) {
+    houseHoldPoints = 14;
+  } else if (numberInHousehold === 2) {
+    houseHoldPoints = 12;
+  } else if (numberInHousehold === 3) {
+    houseHoldPoints = 10;
+  } else if (numberInHousehold === 4) {
+    houseHoldPoints = 8;
+  } else if (numberInHousehold === 5) {
+    houseHoldPoints = 6;
+  } else if (numberInHousehold === 6) {
+    houseHoldPoints = 4;
+  } else if (numberInHousehold > 6) {
+    houseHoldPoints = 2;
+  }
+
+  return houseHoldPoints;
+}
+
+//let carbonFootprintPoints = 0;
+
+function start(houseHoldMembers, houseSize) {
+  const houseHoldPTS = determineHouseHoldPts(houseHoldMembers);
+  const houseSizePTS = determineHouseHoldPts(houseSize);
+  const total = houseHoldPTS + houseSizePTS;
+  cfpData.push([
+    houseHoldMembers,
+    houseSize,
+    houseHoldPTS,
+    houseSizePTS,
+    total,
+  ]);
+}
+
+function displayOutput() {}
+
+start(5, "apt");
+start(4, "large");
+start(3, "meduim");
+start(2, "small");
+start(7, "7 people or more");
+start(6, "6 people");
+start(5, "5 people");
+start(4, "4 people");
+start(3, "3 people");
+start(2, "2 people");
+start(1, "1 person");
+
+displayOutput();
